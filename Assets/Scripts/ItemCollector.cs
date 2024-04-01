3874682a6a8573f6e3e890bd6362ef76411f2ec9
Curcudel Teodor <teocurcudel@gmail.com>
2024-04-01 23:50:27 +0300

    Added animation when collecting a cherries!

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -8,14 +8,18 @@ public class ItemCollector : MonoBehaviour
     private int cherries = 0;
 
     [SerializeField] private Text cherriesText;
+    private Animator anim;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
-        {
-            Destroy(collision.gameObject);
+        {   
+            anim = collision.gameObject.GetComponent<Animator>();
+            anim.SetTrigger("collected");
             cherries++;
             cherriesText.text = "Cherries: " + cherries;
         }
     }
+
+
 }
